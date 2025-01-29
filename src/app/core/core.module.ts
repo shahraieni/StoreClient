279d30f829068878609470e34548f85ddfa69fd6
Mehdi shahraieni <shahraieni.mehdi@gmail.com>
2025-01-29 17:34:17 +0330

    Add navBar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './layers/footer/footer.component';
+import { NavbarComponent } from './layers/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    FooterComponent,
+    NavbarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    FooterComponent,
+    NavbarComponent
   ]
 })
 export class CoreModule { }
